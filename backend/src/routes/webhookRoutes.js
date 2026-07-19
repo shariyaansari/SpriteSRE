07de080d5f5fce8261c455ad30a1ffb9f35b341a
Shariya Ansari <shariyaansari8425@gmail.com>
2026-07-19 15:02:37 +0530

    Re-doing

--- backend/src/routes/webhookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { processGithubWebhook } = require('../controllers/webhookController');
-
-// GitHub will POST here
-router.route('/github')
-    .post(processGithubWebhook);
-
-module.exports = router;
